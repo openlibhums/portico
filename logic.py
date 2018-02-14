0812905c31334a0a495740adfca5ebe7fa509ce5
Andy Byers <andy.byers@openlibhums.org>
2018-02-14 16:52:14 +0000

    Jats now generated fro articles without XML galleys.

--- logic.py
+++ logic.py
@@ -3,13 +3,27 @@ import uuid
 
 from django.shortcuts import get_object_or_404
 from django.conf import settings
+from django.template.loader import render_to_string
 
 from core import files, models as core_models
 from journal import models
 
 
-def generate_jats_metadata(article, temp_file):
-    pass
+def generate_jats_metadata(request, article, article_folder):
+    print('Generating JATS file...')
+    template = 'portico/jats.xml'
+    context = {
+        'article': article,
+        'journal': request.journal
+    }
+
+    rendered_jats = render_to_string(template, context)
+    file_name = '{id}.xml'.format(id=article.pk)
+    full_path = os.path.join(article_folder, file_name)
+
+    with open(full_path, 'w') as file:
+        file.write(rendered_jats)
+        file.close()
 
 
 def prepare_temp_folder(issue=None, article=None):
@@ -32,7 +46,7 @@ def prepare_temp_folder(issue=None, article=None):
     return folder
 
 
-def prepare_article(article, temp_folder):
+def prepare_article(request, article, temp_folder):
     """
     Prepares an article for portico export
     :param article: Article object
@@ -47,7 +61,7 @@ def prepare_article(article, temp_folder):
         xml_galley = galleys.get(file__mime_type__contains='/xml')
         files.copy_file_to_folder(xml_galley.file.self_article_path(), xml_galley.file.uuid_filename, article_folder)
     except core_models.Galley.DoesNotExist:
-        generate_jats_metadata(article, temp_folder)
+        generate_jats_metadata(request, article, article_folder)
 
 
 def prepare_export_for_issue(request):
@@ -64,6 +78,6 @@ def prepare_export_for_issue(request):
     print('Processing {issue}'.format(issue=issue))
 
     for article in issue.articles.all():
-        print('Adding article: {title}'.format(title=article.title))
-        prepare_article(article, temp_folder)
+        print('({pk})Adding article: {title}'.format(pk=article.pk, title=article.title))
+        prepare_article(request, article, temp_folder)
 
