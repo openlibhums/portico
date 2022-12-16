4d2b8ed970ac84f520f754d5fe56856ff4b75268
Andy Byers <a.byers@bbk.ac.uk>
2022-12-16 11:15:44 +0000

    Updated for django 3.2

--- models.py
+++ models.py
@@ -3,5 +3,8 @@ from django.utils import timezone
 
 
 class Delivered(models.Model):
-    issue = models.ForeignKey('journal.Issue')
+    issue = models.ForeignKey(
+        'journal.Issue',
+        on_delete=models.CASCADE,
+    )
     sent_at = models.DateTimeField(default=timezone.now)
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.core.management import call_command
 from django.contrib import messages
 
